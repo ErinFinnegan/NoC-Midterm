44579b1be2bbd8bfafb21c50b3129744c9734e07
ErinFinnegan <eef206@nyu.edu>
2014-03-21 23:27:16 -0400

    rearranged folders, copied working files

---
+++ SpringBreakTests/March21_23/js/sketch.js
@@ -0,0 +1,26 @@
+// The Nature of Code, Daniel Shiffman http://natureofcode.com
+
+
+// an array of ParticleSystems
+var systems = [];
+
+function setup() {
+  var text = createHTML("click to add particle systems");
+  text.position(10,365);
+  canvas = createGraphics(400, 300);
+  canvas.position(160, 125);
+  canvas.class("puke");   //I am not sure what the .class part does
+  //canvas.style("position : relative;");  //this didn't work
+}
+
+function draw() {
+  for(var i=0; i<systems.length; i++){
+  	systems[i].addParticle();
+  	systems[i].run();
+  }  
+ // background(255);
+}
+
+function mousePressed() {
+  systems.push(new ParticleSystem(1, new PVector(mouseX,mouseY)));
+}
