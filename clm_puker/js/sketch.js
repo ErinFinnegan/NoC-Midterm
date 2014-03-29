ffd3f0f44c4c87f25c61732f04369d50cba25859
ErinFinnegan <eef206@nyu.edu>
2014-03-29 15:19:28 -0400

    mouse click detection stopped working

--- clm_puker/js/sketch.js
+++ clm_puker/js/sketch.js
@@ -8,7 +8,7 @@ var rainbow;
 function setup() {
   //var text = createHTML("click to add particle systems");
   //text.position(10,365);
-  canvas = createGraphics(400, 300);
+  canvas = createCanvas(400, 300);
   canvas.position(160, 125);
   canvas.class("puke");   //I am not sure what the .class part does
   //canvas.style("position : relative;");  //this didn't work
@@ -27,11 +27,12 @@ function draw() {
 }
 
 function mouthWasOpen() {
-    systems.push(new ParticleSystem(1, new PVector(mouthX,bottomlip)));
+    systems.push(new ParticleSystem(1, new PVector((mouthX*.5),bottomlip)));
     //console.log('made it to the mouthWasOpen function');
-    //console.log('mouthX, bottomlip ' + mouthX + ',' + bottomlip);
+    //console.log('toplip, bottomlip ' + toplip + ',' + bottomlip);
 }
 
 function mousePressed() {
+  console.log("mouse pressed!");
   console.log('mouseX, mouthX when clicked' + mouseX + ',' + mouthX);
 }
