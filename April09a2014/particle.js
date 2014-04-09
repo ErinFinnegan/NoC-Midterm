76af55c926a4c7174e5585ec368df0f604336ab4
ErinFinnegan <eef206@nyu.edu>
2014-04-09 18:25:53 -0400

    found that I could change the puke by hardcoding...

---
+++ April09a2014/particle.js
@@ -0,0 +1,44 @@
+// The Nature of Code by Daniel Shiffman http://natureofcode.com
+
+// Simple Particle System - a simple Particle class
+var log = false;
+function Particle(position) {
+  this.acceleration = new PVector(0, 0.05);
+  this.velocity = new PVector(myp5.random(-1, 1), myp5.random(-1, 0));
+  this.position = position.get();
+  this.lifespan = 255.0;  
+}
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+}
+
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+}
+
+// Method to display
+Particle.prototype.display = function() {
+  //var whichpuke = vomit;
+  myp5.fill(75, 255, 75);
+  if(!log){
+    console.log("whichpuke =");
+    console.log(whichpuke);
+    log = true;
+  }
+  myp5.image(whichpuke, this.position.x, this.position.y, 50, 50);
+}
+
+ // Is the particle still useful?
+Particle.prototype.isDead = function(){
+  if (this.lifespan < 0.0) {
+      return true;
+    } 
+    else {
+      return false;
+    }
+}
---
+++ April09a2014/sketch.js
@@ -0,0 +1,51 @@
+// The Nature of Code, Daniel Shiffman http://natureofcode.com
+
+
+
+  var rainbow;
+  var money;
+  var candy;
+  var whichpuke;
+  //var puke;
+
+  var s = function( sketch ) {
+  
+
+  // an array of ParticleSystems
+  var systems = [];
+  
+  sketch.mouthWasOpen = function() {
+      systems.push(new ParticleSystem(1, new PVector((mouthX*.5),bottomlip)));
+      //console.log('made it to the mouthWasOpen function');
+      //console.log('toplip, bottomlip ' + toplip + ',' + bottomlip);
+  }
+
+  sketch.setup = function() {
+    //var text = createHTML("click to add particle systems");
+    //text.position(10,365);
+    myp5.canvas = sketch.createCanvas(400, 300);
+    //myp5.canvas.position(160, 125);
+    myp5.canvas.class("p5canvas");   //references the HTML
+    //canvas.style("position : relative;");  //this didn't work
+    //candy = sketch.loadImage("styles/rainbow.png");
+    money = sketch.loadImage("styles/quarter.png");
+    candy = sketch.loadImage("styles/candy.png");
+    rainbow = sketch.loadImage("styles/puke.png");
+    whichpuke =  rainbow;
+  }
+
+  sketch.draw = function() {
+    for(var i=0; i<systems.length; i++){
+    	systems[i].addParticle();
+    	systems[i].run();
+    }  
+    if (mouthopen == true){  //changed the if to a while, made no difference
+      sketch.mouthWasOpen();
+    }
+   // background(255);
+  }
+
+};
+// If its coming from outside the sketch you can use p5.thing, but inside the sketch its sketch.thing
+
+var myp5 = new p5(s, 'p5canvas');
